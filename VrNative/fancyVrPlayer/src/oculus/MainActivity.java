b8292884acdbe8a311182bd00a5f2295d653072a
sssa2000 <sssa2000@163.com>
2016-03-15 01:40:12 +0800

    加入了异步的文件夹遍历

--- VrNative/fancyVrPlayer/src/oculus/MainActivity.java
+++ VrNative/fancyVrPlayer/src/oculus/MainActivity.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 
 import com.fancyTech.fancyVrPlayer.R;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -27,17 +28,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.AdapterView.OnItemClickListener;
 
+
 public class MainActivity extends Activity{
 	public static final String TAG = "FancyTech";
-
-
+	private ListView lv;
+	private Button button_scan;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
 		
-        final ListView lv=(ListView)findViewById(R.id.listView1);
+        lv=(ListView)findViewById(R.id.listView1);
         lv.setOnItemClickListener(new OnItemClickListener()
         {
 			@Override
@@ -50,18 +53,61 @@ public class MainActivity extends Activity{
         });
         
         //扫描的按钮
-		Button button_scan=(Button)findViewById(R.id.button2);
+        button_scan=(Button)findViewById(R.id.button2);
 		button_scan.setOnClickListener(new View.OnClickListener() 
 		{
 			public void onClick(View v)
 			{
 				
+				final EnumDiskTask myTask =  new EnumDiskTask();
+				myTask.execute("");
+				button_scan.setEnabled(false);
+				
 				ArrayList<String> fs=DiskEnum.scanVideoAndUpdateUI();
-				ArrayAdapter<String> adpt=new ArrayAdapter<String> (MainActivity.this,android.R.layout.simple_list_item_1,fs);
-				lv.setAdapter(adpt);
+				
 			}
 		});
 
     }
 
+    //“启动任务执行的输入参数”、“后台任务执行的进度”、“后台计算结果的类型”
+    private class EnumDiskTask extends AsyncTask<String,Integer,String>{
+       	
+    	private ArrayList<String> enumResult;
+    	//onPreExecute方法用于在执行后台任务前做一些UI操作
+    	@Override
+    	protected void onPreExecute() {
+    		
+    	}
+    	
+    	//doInBackground方法内部执行后台任务,不可在此方法内修改UI
+    	@Override
+    	protected String doInBackground(String...param) {
+    		
+    		enumResult=DiskEnum.scanVideoAndUpdateUI();
+    		return null;
+    	}
+    	
+    	//onProgressUpdate方法用于更新进度信息
+    	@Override
+    	protected void onProgressUpdate(Integer... progresses) {
+    				
+    	}
+    			
+    	//onPostExecute方法用于在执行完后台任务后更新UI,显示结果
+    	@Override
+    	protected void onPostExecute(String result) {
+    		ArrayAdapter<String> adpt=new ArrayAdapter<String> (MainActivity.this,android.R.layout.simple_list_item_1,enumResult);
+			lv.setAdapter(adpt);
+			button_scan.setEnabled(true);
+			Toast.makeText(getApplicationContext(), "Scan Done! res num="+enumResult.size(), 0).show();  
+    	}
+    			
+    	//onCancelled方法用于在取消执行中的任务时更改UI
+    	@Override
+    	protected void onCancelled() {
+    				
+    				
+    	}
+    }
 }
